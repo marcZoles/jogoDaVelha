590f425a4e0bf2f94ad0a22e3eb4bbe3e7752f5c
Thais <2025106239@unibrasil.com>
2025-08-13 21:17:36 -0300

    atualizacaoDACERTOPFVR

--- jogoDaVelha.main/Program.cs
+++ jogoDaVelha.main/Program.cs
@@ -1,8 +1,91 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-//Thais aaaaaaaaaaaaaaaaaaaa
-// Funciona, amem!
+﻿bool b = false;
 
-//testeeeeeeeeeeeeeeeee jose →→→
+Console.WriteLine("|=======================================================|");
 
-//teste Bianca
+Console.WriteLine("|||||||||||||||||||---MENU PRINCIPAL---||||||||||||||||||");
+Console.WriteLine("|=======================================================|");
+
+while (!b)
+{
+
+    Console.WriteLine("Sempre que quiser sair do jogo aperte 'S'");
+    Console.WriteLine("Escolha o modo de jogo: ");
+    Console.WriteLine("1 - Jogador vs Jogador");
+    Console.WriteLine("2 - Jogador vs Computador");
+    Console.WriteLine("3 - Sair do jogo");
+    Console.Write("Selecione: ");
+
+    string modoDeJogo = Console.ReadLine();
+    int modoDeJogoConvert = Convert.ToInt32(modoDeJogo);
+
+
+    Console.Clear();
+   
+
+    switch (modoDeJogoConvert)
+    {
+        case 1:
+            InicieJogadorVSJogador();
+         
+            
+            break;
+        case 2:
+            InicieJogadorVSPc();
+            break;
+        case 3:
+            Console.WriteLine("Jogo encerrado!");
+            break;
+        default:
+            Console.WriteLine("Tecla inválida :(");
+            break;
+    }
+
+    static void InicieJogadorVSJogador()
+    {
+        Console.WriteLine("|====================================|");
+        Console.WriteLine("|Modo selecionado: Jogador vs Jogador|");
+        Console.WriteLine("|====================================|");
+    }
+
+    static void InicieJogadorVSPc()
+    {
+        Console.WriteLine("|=======================================|");
+        Console.WriteLine("|Modo selecionado: Jogador vs computador|");
+        Console.WriteLine("|=======================================|");
+        Console.WriteLine("Selecione a dificuldade do jogo: ");
+        Console.WriteLine("D - Dificil");
+        Console.WriteLine("F - Facil");
+        string dificuldade = Console.ReadLine();
+        Console.Clear();
+        switch (dificuldade)
+        {
+            case "F":
+                InicieModoFacil();
+                break;
+            case "D":
+                InicieModoDificil();
+                break;
+            default:
+                Console.WriteLine("Opção invalida");
+                break;
+        }
+
+    }
+    static void InicieModoFacil()
+    {
+        Console.WriteLine("==Modo Fácil===");
+    }
+
+    static void InicieModoDificil()
+    {
+        Console.WriteLine("==Modo Dificil==");
+    }
+
+
+    if (modoDeJogoConvert == 3)
+    {
+        b = true;
+    }
+}
+
+// Teste pra ve se o bagua me aceita la

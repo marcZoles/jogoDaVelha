772481bccc162aa5eaa47cd6aed428c694845d5c
Thais <2025106239@unibrasil.com>
2025-08-20 18:48:49 -0300

    fudeu

--- jogoDaVelha.main/Program.cs
+++ jogoDaVelha.main/Program.cs
@@ -1,7 +1,7 @@
 ﻿// =======================================================|
 
 // TRABALHO DE CONCLUSÃO DE MATÉRIA - JOGO DA VELHA EM C#
-// FEITO POR: Gabriel Marczal; Bianca Michoski; Thays Colaço; José Guides
+// FEITO POR: Gabriel Marczal; Bianca Michoski; Thais Colaço; José Guides
 
 // ========================================================|
 
@@ -26,24 +26,31 @@ int rankingJ2 = 0;
 
 // ========================================================|
 
+while (!b)
+{
+    Console.WriteLine("|=======================================================|");
+    Console.WriteLine("|||||||||||||||||||---MENU PRINCIPAL---||||||||||||||||||");
+    Console.WriteLine("|=======================================================|");
 
-Console.WriteLine("|=======================================================|");
-Console.WriteLine("|||||||||||||||||||---MENU PRINCIPAL---||||||||||||||||||");
-Console.WriteLine("|=======================================================|");
+    Console.WriteLine("Bem vindo ao jogo da velha!");
+    Console.WriteLine("Escolha o modo de jogo: ");
+    Console.WriteLine("1 - Jogador vs Jogador");
+    Console.WriteLine("2 - Jogador vs Computador");
+    Console.WriteLine("3 - Exibir Ranking");
+    Console.WriteLine("4 - Sair do Jogo");
+    Console.Write("Selecione: ");
 
-Console.WriteLine("Bem vindo ao jogo da velha!");
-Console.WriteLine("Escolha o modo de jogo: ");
-Console.WriteLine("1 ►Jogador vs Jogador");
-Console.WriteLine("2 ►Jogador vs Computador");
-Console.WriteLine("3 ►Exibir Ranking");
-Console.WriteLine("4 ►Sair do Jogo");
-Console.Write("Selecione: ");
+    string modoDeJogo = Console.ReadLine();
+    int modoDeJogoConvert;
+    if (!int.TryParse(modoDeJogo, out modoDeJogoConvert))
+    {
+         /* Esse if está em branco pois ele serve apenas para fazer a verificação se o usuário digitou um NUMERO
+          * pq ele vai executar o código que está no switch, entao nao precisa de uma mensagem de erro
+          */
+    }                                         
 
-string modoDeJogo = Console.ReadLine();
-int modoDeJogoConvert = Convert.ToInt32(modoDeJogo);
-   
     Console.Clear();
-   
+
 
     switch (modoDeJogoConvert) // Problema do loop infinito foi corrigido pelo José & Thays, agora tá funcionando filé || - marcZ
     {
@@ -69,118 +76,155 @@ int modoDeJogoConvert = Convert.ToInt32(modoDeJogo);
             break;
 
         case 5:
-        JogoDaVelha();
+            JogoDaVelha();
             break;
 
         default:
 
-            Console.WriteLine("Tecla inválida, tente novamente.");
-            break;
+            Console.WriteLine("Opção inválida! Tente novamente digitando um valor númerico ou uma das opções abaixo");
+            continue; /* O continue, como o nome diz continua o loop (esse codigo esta dentro de um while, para que o usuário
+                       * consiga digitar outra opção válida
+                      */
     }
 
 
-static void JogoDaVelha()
-{
-    string jogador1 = "X";
-    string jogador2 = "O";
-    int rankingJ1 = 0;
-    int rankingJ2 = 0;
-    string[,] tabuleiro = new string[3, 3];
-    string turno = "X";
-    int contadorTurnos = 0;
-    int mioloDoTabuleiro = 1;
-    string mensagemVitoria = $"Jogador {turno} VENCEUUUUUUUU ::))))";
-    
-    //for que preenche a matriz com os numeros de 1 a 9, ou seja, o miolo do tabuleiro
-    for (int i = 0; i < tabuleiro.GetLength(0); i++)//get lenght (pesquisar melhor sobre isso)
-    { 
-       for (int j = 0; j < tabuleiro.GetLength(1); j++)
-        {
-            tabuleiro[i, j] = mioloDoTabuleiro.ToString();//melhorar essa conversao para string se possível
-            mioloDoTabuleiro++;
-        }
-
-    }
-    // esse segundo for é quem imprime a matriz na tela
-    for (int i = 0; i < tabuleiro.GetLength(0); i++)
+    static void JogoDaVelha()
     {
-        for (int j = 0; j < tabuleiro.GetLength(1); j++)
-        {
-            Console.Write(tabuleiro[i, j]);
-            if (j < 2) Console.Write("  | ");
-            
-        }
-        Console.WriteLine();
-        if (i < 2) Console.WriteLine("---+----+----");
+        string jogador1 = "X";
+        string jogador2 = "O";
+        int rankingJ1 = 0;
+        int rankingJ2 = 0;
+        string[,] tabuleiro = new string[3, 3];
+        string turno = "X";
+        int contadorTurnos = 0;
+        int mioloDoTabuleiro = 1;
+        string mensagemVitoria = $"Jogador {turno} VENCEUUUUUUUU ::))))";
 
-    }
-
-    // Esse while é o que controla o jogo, ele vai rodar enquanto o contador de turnos for menor que 9, ou seja, enquanto houver jogadas possíveis
-    while (contadorTurnos < 9)
-    {
-        Console.WriteLine("|====================================|");
-        Console.WriteLine($"|========| O jogador {turno} joga |========|");
-        Console.WriteLine("|====================================|");
-        string jogada = Console.ReadLine();
-
-        // Aqui eu estou verificando se a jogada é válida, ou seja, se o jogador digitou um número de 1 a 9
-        // Se for valida ele atualiza o tabuleiro
-        for (int i = 0; i < tabuleiro.GetLength(0); i++)
-                                                     
+        //for que preenche a matriz com os numeros de 1 a 9, ou seja, o miolo do tabuleiro
+        for (int i = 0; i < tabuleiro.GetLength(0); i++)//get lenght (pesquisar melhor sobre isso)
         {
             for (int j = 0; j < tabuleiro.GetLength(1); j++)
             {
-                if(jogada == tabuleiro[i, j])
-                {
-                    tabuleiro[i, j] = turno;
-
-                }
+                tabuleiro[i, j] = mioloDoTabuleiro.ToString();//melhorar essa conversao para string se possível
+                mioloDoTabuleiro++;
             }
 
         }
-
-        // Aqui eu estou imprimindo o tabuleiro atualizado na tela
+        // esse segundo for é quem imprime a matriz na tela
         for (int i = 0; i < tabuleiro.GetLength(0); i++)
         {
             for (int j = 0; j < tabuleiro.GetLength(1); j++)
             {
                 Console.Write(tabuleiro[i, j]);
                 if (j < 2) Console.Write("  | ");
+
             }
             Console.WriteLine();
             if (i < 2) Console.WriteLine("---+----+----");
+
         }
 
-
-        //comeco a verificar se o jogador ganhou, ou seja, se ele conseguiu fazer uma linha, coluna ou diagonal com o mesmo simbolo
-        // ta fucionando, agora é so replicar para TODAS as as possibilidades de vitoria (emoji de caveira)
-        if (contadorTurnos > 3)
+        // Esse while é o que controla o jogo, ele vai rodar enquanto o contador de turnos for menor que 9, ou seja, enquanto houver jogadas possíveis
+        while (contadorTurnos < 9)
         {
-           
-            for (int i = 1; i < 3; i++) //ele ira verificar as 3 linhas (0, 1, 2)
+            Console.WriteLine("|====================================|");
+            Console.WriteLine($"|========| O jogador {turno} joga |========|");
+            Console.WriteLine("|====================================|");
+            string jogada = Console.ReadLine();
+
+            // Aqui eu estou verificando se a jogada é válida, ou seja, se o jogador digitou um número de 1 a 9
+            // Se for valida ele atualiza o tabuleiro
+            for (int i = 0; i < tabuleiro.GetLength(0); i++)
+
             {
-                if (tabuleiro[i, 0] != "" && // Verifica se a primeira linha não está vazia - Thais
-                    tabuleiro[i, 0] == tabuleiro[i, 1] && // verifica se a primeira linha é igual a segunda - Thais
-                    tabuleiro[i, 1] == tabuleiro[i, 2]) // verifica se a segunda linha é igual a terceira - Thais
+                for (int j = 0; j < tabuleiro.GetLength(1); j++)
                 {
-                    Console.WriteLine(mensagemVitoria);
-                if (turno == "X")
+                    if (jogada == tabuleiro[i, j])
                     {
-                        rankingJ1++;
-                        Console.WriteLine($"O jogador {turno} tem "+i+" vitorias");
-                        
-                    } else
-                    {
-                        rankingJ2++;
-                        Console.WriteLine($"O jogaodor {turno} tem " + i + " vitorias");
+                        tabuleiro[i, j] = turno;
+
                     }
-                    
+                }
+
+            }
+
+            // Aqui eu estou imprimindo o tabuleiro atualizado na tela
+            for (int i = 0; i < tabuleiro.GetLength(0); i++)
+            {
+                for (int j = 0; j < tabuleiro.GetLength(1); j++)
+                {
+                    Console.Write(tabuleiro[i, j]);
+                    if (j < 2) Console.Write("  | ");
+                }
+                Console.WriteLine();
+                if (i < 2) Console.WriteLine("---+----+----");
+            }
+
+
+            //comeco a verificar se o jogador ganhou, ou seja, se ele conseguiu fazer uma linha, coluna ou diagonal com o mesmo simbolo
+            // ta fucionando, agora é so replicar para TODAS as as possibilidades de vitoria (emoji de caveira)
+            if (contadorTurnos > 3)
+            {
+
+                for (int i = 1; i < 3; i++) //ele ira verificar as 3 linhas (0, 1, 2)
+                {
+                    if (tabuleiro[i, 0] != "" && // Verifica se a primeira linha não está vazia - Thais
+                        tabuleiro[i, 0] == tabuleiro[i, 1] && // verifica se a primeira linha é igual a segunda - Thais
+                        tabuleiro[i, 1] == tabuleiro[i, 2]) // verifica se a segunda linha é igual a terceira - Thais
+                    {
+                        Console.WriteLine(mensagemVitoria);
+                        if (turno == "X")
+                        {
+                            rankingJ1++;
+                            Console.WriteLine($"O jogador {turno} tem " + i + " vitorias");
+
+                        }
+                        else
+                        {
+                            rankingJ2++;
+                            Console.WriteLine($"O jogaodor {turno} tem " + i + " vitorias");
+                        }
+
                         // colunas
                         for (int j = 1; j < 3; j++)
-                    {
-                        if (tabuleiro[0, j] != "" &&
-                            tabuleiro[0, j] == tabuleiro[1,j]&&
-                            tabuleiro[1,j] == tabuleiro[2, j])
+                        {
+                            if (tabuleiro[0, j] != "" &&
+                                tabuleiro[0, j] == tabuleiro[1, j] &&
+                                tabuleiro[1, j] == tabuleiro[2, j])
+                            {
+                                Console.WriteLine(mensagemVitoria);
+                                if (turno == "X")
+                                {
+                                    rankingJ1++;
+                                    Console.WriteLine($"O jogaodor {turno} tem " + i + " vitorias");
+                                }
+                                else
+                                {
+                                    rankingJ2++;
+                                    Console.WriteLine($"O jogaodor {turno} tem " + i + " vitorias");
+                                }
+                            }
+                        }
+                        // diagonais
+                        if (tabuleiro[0, 0] != " " &&
+                            tabuleiro[0, 0] == tabuleiro[1, 1] &&
+                            tabuleiro[1, 1] == tabuleiro[2, 2])
+                        {
+                            Console.WriteLine(mensagemVitoria);
+                            if (turno == "X")
+                            {
+                                rankingJ1++;
+                                Console.WriteLine($"O jogaodor {turno} tem " + i + " vitorias");
+                            }
+                            else
+                            {
+                                rankingJ2++;
+                                Console.WriteLine($"O jogaodor {turno} tem " + i + " vitorias");
+                            }
+                        }
+                        if (tabuleiro[0, 2] != " " &&
+                            tabuleiro[0, 2] == tabuleiro[1, 1] &&
+                            tabuleiro[1, 1] == tabuleiro[2, 0])
                         {
                             Console.WriteLine(mensagemVitoria);
                             if (turno == "X")
@@ -195,116 +239,88 @@ static void JogoDaVelha()
                             }
                         }
                     }
-                    // diagonais
-                    if (tabuleiro[0, 0] != " " &&
-                        tabuleiro[0, 0] == tabuleiro[1, 1] &&
-                        tabuleiro[1, 1] == tabuleiro[2, 2])
-                    {
-                        Console.WriteLine(mensagemVitoria);
-                        if (turno == "X")
-                        {
-                            rankingJ1++;
-                            Console.WriteLine($"O jogaodor {turno} tem " + i + " vitorias");
-                        }
-                        else
-                        {
-                            rankingJ2++;
-                            Console.WriteLine($"O jogaodor {turno} tem " + i + " vitorias");
-                        }
-                    } 
-                    if(tabuleiro[0, 2] != " " &&
-                        tabuleiro[0, 2] == tabuleiro[1, 1] &&
-                        tabuleiro[1, 1] == tabuleiro[2, 0])
-                    {
-                        Console.WriteLine(mensagemVitoria);
-                        if (turno == "X")
-                        {
-                            rankingJ1++;
-                            Console.WriteLine($"O jogaodor {turno} tem " + i + " vitorias");
-                        }
-                        else
-                        {
-                            rankingJ2++;
-                            Console.WriteLine($"O jogaodor {turno} tem " + i + " vitorias");
-                        }
-                    }
                 }
             }
-        }
             contadorTurnos++;
-        if(contadorTurnos % 2 == 0) 
-        {
-            turno = "X";
+            if (contadorTurnos % 2 == 0)
+            {
+                turno = "X";
+            }
+            else
+            {
+                turno = "O";
+            }
         }
-        else
-        {
-            turno = "O";
-        }
+
+
     }
-
-
-}
-// ========================================================================================================//
+    // ========================================================================================================//
     static void InicieJogadorVSJogador(char[,] tabuleiro)
     {
         Console.WriteLine("|====================================|");
         Console.WriteLine("|Modo selecionado: Jogador vs Jogador|");
         Console.WriteLine("|====================================|");
-        
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    tabuleiro[i, j] = ' ';
-                }
-            }
 
-
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    Console.Write(tabuleiro[i, j]);
-                    if (j < 2) Console.Write(" | ");
-                }
-                Console.WriteLine();
-                if (i < 2) Console.WriteLine("---+---+---");
-
-            }
-
-    }
-// ========================================================================================================//
-
-static void InicieJogadorVSPc()
-    {
-        Console.WriteLine("|=======================================|");
-        Console.WriteLine("|Modo selecionado: Jogador vs computador|");
-        Console.WriteLine("|=======================================|");
-        Console.WriteLine("Selecione a dificuldade do jogo: ");
-        Console.WriteLine("D - Dificil");
-        Console.WriteLine("F - Facil");
-        string dificuldade = Console.ReadLine();
-        Console.Clear();
-        switch (dificuldade)
+        for (int i = 0; i < 3; i++)
         {
-            case "F":
-                InicieModoFacil();
-                break;
-            case "D":
-                InicieModoDificil();
-                break;
-            default:
-                Console.WriteLine("Opção invalida");
-                break;
+            for (int j = 0; j < 3; j++)
+            {
+                tabuleiro[i, j] = ' ';
+            }
+        }
+
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write(tabuleiro[i, j]);
+                if (j < 2) Console.Write(" | ");
+            }
+            Console.WriteLine();
+            if (i < 2) Console.WriteLine("---+---+---");
+
         }
 
     }
+    // ========================================================================================================//
 
-// Ainda não estamos utilizando esses métodos, mas eles serão usados no futuro || Favor não apagar
+    static void InicieJogadorVSPc()
+    {
+        
+        {
+            Console.Clear();
+            Console.WriteLine("|=======================================|");
+            Console.WriteLine("|Modo selecionado: Jogador vs computador|");
+            Console.WriteLine("|=======================================|");
+            Console.WriteLine("Selecione a dificuldade do jogo: ");
+            Console.WriteLine("D - Dificil");
+            Console.WriteLine("F - Facil");
+            string dificuldade = Console.ReadLine().ToUpper();
+            { 
+            
 
-// ========================================================================================================//
+                switch (dificuldade)
+                {
+                    case "F":
+                        InicieModoFacil();
+                        break;
+                    case "D":
+                        InicieModoDificil();
+                        break;
+                    default:
+                        Console.WriteLine("Opção invalida");
+                        break;
+                }
+                
+            }
+        }
+    }
+    // Ainda não estamos utilizando esses métodos, mas eles serão usados no futuro || Favor não apagar
 
-static void InicieModoFacil()
+    // ========================================================================================================//
+
+    static void InicieModoFacil()
     {
         Console.WriteLine("==Modo Fácil===");
     }
@@ -323,7 +339,7 @@ static void InicieModoFacil()
     {
         b = true;
     }
-
+}
 
 
 
